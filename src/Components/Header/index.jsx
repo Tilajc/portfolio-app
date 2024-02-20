eea54559b41d2007486000b51026d65a9175790f
Juan Cruz Tilatti <jctilatti@gmail.com>
2024-02-20 02:42:52 -0300

    PR-0: Finished the first header version

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import styles from './header.module.css';
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.infoContainer}>
+        <img src="img/logo_portfolio.png" alt="Profile Photo" className={styles.photo} />
+        <h1 className={styles.name}>Juan Cruz Tilatti</h1>
+        <p className={styles.role}>
+          {'<'}
+          <span></span>
+        </p>
+      </div>
+      <div className={styles.aContainer}>
+        <a href="/projects" className={styles.a}>
+          Projects
+        </a>
+        <a href="/about" className={styles.a}>
+          About Me
+        </a>
+        <a href="/contact" className={styles.a}>
+          Contact
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import Projects from '../Projects';
+import About from '../About';
+import Contact from '../Contact';
+import styles from './layout.module.css';
 
 function Layout() {
-  return (
-    <main>
-      <h1>Layout</h1>
-    </main>
-  );
+  let currentScreen;
+  switch (window.location.pathname) {
+    case '/contact':
+      currentScreen = <Contact />;
+      break;
+    case '/about':
+      currentScreen = <About />;
+      break;
+    case '/projects':
+      currentScreen = <Projects />;
+      break;
+    default:
+      currentScreen = <Projects />;
+      break;
+  }
+
+  return <main className={styles.main}>{currentScreen}</main>;
 }
 
 export default Layout;
