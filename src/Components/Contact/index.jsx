9056af0dbc50c5b304dbcdb1e5cdbb675b098242
Juan Cruz Tilatti <jctilatti@gmail.com>
2024-03-06 00:46:20 -0300

    Added footer

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import styles from './contact.module.css';
 
 const Contact = () => {
-  return <h2>Contact</h2>;
+  const copyEmailToClipboard = () => {
+    const emailElement = document.getElementById('email');
+    const emailText = emailElement.innerText;
+    const textarea = document.createElement('textarea');
+
+    textarea.value = emailText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+
+    alert('¡Email copiado al portapapeles!');
+  };
+
+  return (
+    <section className={styles.section}>
+      <p>email:</p>
+      <div className={styles.emailContainer}>
+        <p id="email">jctilatti@gmail.com</p>
+        <img src="img/copy.png" alt="copy" className={styles.copy} onClick={copyEmailToClipboard} />
+      </div>
+      {/* <form action="submit">
+        <label htmlFor="email">Email</label>
+        <input type="email" />
+        <label htmlFor="message">Message</label>
+        <textarea id="message"></textarea>
+      </form> */}
+    </section>
+  );
 };
 
 export default Contact;
--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import styles from './footer.module.css';
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <p>You can check my social media here!</p>
+      <img src="img/github.png" alt="Github Logo" className={styles.img} />
+      <img src="img/linkedin.png" alt="Linkedin Logo" className={styles.img} />
+    </footer>
+  );
 };
 
 export default Footer;
