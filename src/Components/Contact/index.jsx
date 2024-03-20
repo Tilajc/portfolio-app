2a51df8068b0471f6638129abc7a98404d74489c
Juan Cruz Tilatti <jctilatti@gmail.com>
2024-03-20 02:09:30 -0300

    Added some animations when you copy the email

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './contact.module.css';
 
 const Contact = () => {
+  const [copied, setCopied] = useState(false);
+  const [copiedCount, setCopiedCount] = useState(0);
+  const [timeOutID, setTimeoutID] = useState(null);
+
   const copyEmailToClipboard = () => {
     const emailElement = document.getElementById('myEmail');
     const emailText = emailElement.innerText;
@@ -13,15 +17,44 @@ const Contact = () => {
     document.execCommand('copy');
     document.body.removeChild(textarea);
 
-    alert('Email copied correctly!');
+    setCopied(true);
+    setCopiedCount(copiedCount + 1);
+
+    if (timeOutID) {
+      clearTimeout(timeOutID);
+    }
+
+    const id = setTimeout(finishCopied, 3000);
+    setTimeoutID(id);
+  };
+
+  const finishCopied = () => {
+    setCopied(false);
+    setCopiedCount(0);
   };
 
   return (
     <section className={styles.section}>
       <p>Copy my email here!</p>
-      <div className={styles.emailContainer}>
-        <p id="myEmail">jctilatti@gmail.com</p>
-        <img src="img/copy.png" alt="copy" className={styles.copy} onClick={copyEmailToClipboard} />
+      <div className={styles.emailCopyContainer}>
+        <button className={styles.emailButton} onClick={copyEmailToClipboard}>
+          <p id="myEmail">jctilatti@gmail.com</p>
+          <img src="img/copy.png" alt="copy" className={styles.copy} />
+        </button>
+        <p className={copied && copiedCount === 1 ? styles.emailCopied : styles.none}>Copied!</p>
+        <p className={copied && copiedCount === 2 ? styles.emailCopied : styles.none}>
+          Double Copied!
+        </p>
+        <p className={copied && copiedCount === 3 ? styles.emailCopied : styles.none}>
+          Triple Copied!
+        </p>
+        <p
+          className={
+            copied && copiedCount > 3 ? `${styles.emailCopied} ${styles.vibrate}` : styles.none
+          }
+        >
+          SUPER COPIED!
+        </p>
       </div>
       <p>or send me an email by the form</p>
       <form action="submit" className={styles.form}>
